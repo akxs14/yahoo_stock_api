3bdc7d17b53f7650e416d3eb1ec2b99fc0ec5371
angelos <angelos.kapsimanis@gmail.com>
2017-09-04 00:59:07 +0200

    scratch

--- compile_stock_list.py
+++ compile_stock_list.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 
-from yahoo_finance import Share
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+from time import sleep
+import pandas as pd
 
-google = Share('YHOO')
-print google.get_open()
+ticker_symbols = ["1736:JP", "6758:JP", "8159:JP", "8923:JP", "6960:JP", "1736:JP",
+                  "1758:JP", "1799:JP", "8066:JP", "4202:JP", "6954:JP"]
+
+def fetch_stock_data(symbol):
+    sleep(0.5)
+    resp = requests.get("https://www.bloomberg.com/quote/" + symbol)
+
+    if resp.ok:
+      html = BeautifulSoup(resp.text, "html.parser")
+    else:
+      print "error: {}".format(resp.status_code)
+
+    data_table = html.body.find('div', attrs={'class':'data-table_detailed'})
+    entries = data_table.find_all('div')
+
+    pe_ratio = entries[23].text     # price per earnings ratio
+    e_per_share = entries[26].text  # earnings per share
+    price = entries[2].text         # price
+    market_cap = entries[29].text   # market capitalization
+
+    return {"symbol": symbol, "price": price, "pe": pe_ratio, "es": e_per_share, 
+            "cap": market_cap}
+
+
+stock_data = []
+for symbol in ticker_symbols:
+  stock_data.append(fetch_stock_data(symbol))
+
+df = pd.DataFrame(stock_data)
+print df.describe()
